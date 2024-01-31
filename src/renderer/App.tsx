c2851ca9117136250ca4824ec0d982b83bb9921b
oscar <oscardavilasampedro@gmail.com>
2024-01-31 13:02:56 +0100

    Cambios menores en la interfaz

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,32 +1,53 @@
+import { Button, TextField, FormControl, FormLabel } from '@mui/material';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import Button from '@mui/material/Button';
-import readStream from '../utils/StreamUtil';
+import React, { useState } from 'react';
 import './App.css';
+import readStream from '../utils/StreamUtil';
 
-function Hello() {
+function Home() {
+  const [username, setUsername] = useState('');
+  const [usernameError, setUsernameError] = useState(false);
+
+  function showUserGames() {
+    const stream = fetch(`https://lichess.org/api/games/user/${username}`, {
+      headers: { Accept: 'application/x-ndjson' },
+    });
+
+    const onMessage = (obj: any) => console.log(obj);
+    const onComplete = () => console.log('The stream has completed');
+
+    stream
+      .then(readStream(onMessage))
+      .then(onComplete)
+      .catch((error) => {
+        console.error('Error: ', error);
+      });
+  }
+
+  const handleSubmit = () => {
+    setUsernameError(false);
+
+    if (username === '') {
+      setUsernameError(true);
+    } else {
+      showUserGames();
+    }
+  };
+
   return (
-    <div>
-      <Button
-        variant="contained"
-        onClick={() => {
-          const stream = fetch('https://lichess.org/api/games/user/neio', {
-            headers: { Accept: 'application/x-ndjson' },
-          });
-
-          const onMessage = (obj: any) => console.log(obj);
-          const onComplete = () => console.log('The stream has completed');
-
-          stream
-            .then(readStream(onMessage))
-            .then(onComplete)
-            .catch((error) => {
-              console.error('Error:', error);
-            });
-        }}
-      >
-        Hola
+    <FormControl>
+      <FormLabel>Introduzca el nombre de usuario: </FormLabel>
+      <TextField
+        required
+        value={username}
+        error={usernameError}
+        label="Nombre de usuario"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <Button variant="contained" onClick={handleSubmit}>
+        Importar partidas
       </Button>
-    </div>
+    </FormControl>
   );
 }
 
@@ -34,7 +55,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
